ab64d5efc790f5e0eb6bf4ce0a487d671c8efd25
jimikid <jimikid@hotmail.com>
2017-06-06 13:44:55 +0900

    init

---
+++ Scripts_FEA/main_run.py
@@ -0,0 +1,13 @@
+"""
+Created on 03/18/2016, @author: sbaek
+  V00
+    - initial release
+"""
+
+
+import os
+path='C:/Users/sbaek/WorkSpace/scripts/'
+        
+os.system(path+'main_FEA_analysis.py')
+os.system(path+'main_FEA_Rac.py')
+
